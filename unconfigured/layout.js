140cb2d3df17177b642386b3f91750452d815fca
Ben Englert <ben@M5.local>
2009-11-18 19:01:04 -0500

    Latest version of all slides

---
+++ unconfigured/layout.js
@@ -0,0 +1,44 @@
+{
+    "id" : "slide",
+        "type" : "ClutterGroup",
+        "visible" : true,
+        "children" : [
+            {
+                "id" : "yodawg",
+                    "type" : "ClutterText",
+                    "x" : 0,
+                    "y" : 0,
+                    "text" : "Yo dawg",
+                    "color" : "white",
+                    "font_name" : "Mono 128",
+                    "width" : 1920,
+                    "height": 1080,
+                    "visible" : true
+                    },
+            {
+                "id" : "notright",
+                    "type" : "ClutterText",
+                    "x" : 0,
+                    "y" : 360,
+                    "text" : "You need to be configured at",
+                    "color" : "white",
+                    "font_name" : "Mono 72",
+                    "width" : 1920,
+                    "height": 1080,
+                    "visible" : true
+                    },
+            {
+                "id" : "url",
+                    "type" : "ClutterText",
+                    "x" : 50,
+                    "y" : 700,
+                    "text" : "http://dds-master.ccs.neu.edu/dds/admin/",
+                    "color" : "white",
+                    "font_name" : "Mono 48",
+                    "width" : 1920,
+                    "height": 1080,
+                    "visible" : true
+                    }
+
+    ]
+        }
