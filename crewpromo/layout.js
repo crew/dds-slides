a3cf050f2445e107597b42cc14d9d7f0b814762c
Will Nowak <compbrain@gmail.com>
2009-11-22 18:45:50 -0500

    add crew promo slide

---
+++ crewpromo/layout.js
@@ -0,0 +1,18 @@
+{
+    "id" : "slide",
+        "type" : "ClutterGroup",
+        "visible" : true,
+        "children" : [
+            {
+                "id" : "townhall",
+                    "type" : "ClutterTexture",
+                    "filename" : "gettingstuffdon.jpg",
+                    "x" : 0,
+                    "y" : 0,
+                    "width" : 1920,
+                    "height": 1080,
+                    "visible" : true
+                    }
+
+    ]
+        }
