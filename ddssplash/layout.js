4f9336e510853868329ab88fc39ba1f8d4a765eb
Will Nowak <compbrain@gmail.com>
2010-01-20 22:47:24 -0500

    Add DDS Splash slide

---
+++ ddssplash/layout.js
@@ -0,0 +1,18 @@
+{
+    "id" : "slide",
+        "type" : "ClutterGroup",
+        "visible" : true,
+        "children" : [
+            {
+                "id" : "splash",
+                    "type" : "ClutterTexture",
+                    "filename" : "dds-logo.png",
+                    "x" : 0,
+                    "y" : 0,
+                    "width" : 1920,
+                    "height": 1080,
+                    "visible" : true
+                    }
+
+    ]
+        }
