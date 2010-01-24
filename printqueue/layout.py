9a921a8592212c6ba8b1d4677222e4912c039482
Ian Helmke <ihelmke@aurora.(none)>
2010-01-23 19:17:03 -0500

    Made something resembling a printqueue slide in some way. Added a shell script that automatically packages and moves the layout and manifest files into the ~/.dds/slides/1 directory.

--- printqueue/layout.py
+++ printqueue/layout.py
@@ -12,8 +12,8 @@ name2codepoint['#39'] = 39
 # regex lib for stripping HTML tags
 import re
 
-SCREEN_HEIGHT = 1024
-SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 1200
+SCREEN_WIDTH = 1920
 
 class PrintDisplay(baseslide.BaseSlide):
   def __init__(self, dataURL):
@@ -21,7 +21,10 @@ class PrintDisplay(baseslide.BaseSlide):
         given url to get the printer data. """
     baseslide.BaseSlide.__init__(self)
     self.parsedata(dataURL)
+    # Stores the information for all the elements of each row
+    self.rows = list()
     self.makeslide()
+    self.render()
     self.dataURL = dataURL
 
   def setupslide(self):
@@ -30,7 +33,8 @@ class PrintDisplay(baseslide.BaseSlide):
 
   def refresh(self, dataURL):
     self.group.remove_all()
-    
+    self.parsedata(dataURL)
+    self.makeslide()
 
   def parsedata(self, url):
     """ Parse data from the given URL, and populate data objects
@@ -48,36 +52,42 @@ class PrintDisplay(baseslide.BaseSlide):
 
   def makeslide(self):
     """ Adds the json print feed information to this slide. """
+    
 
     # Make a white rectangle to give the slide a white background
-    # (our current preferred solution
+    # (our current preferred solution)
     background = clutter.Rectangle()
     background.set_color(clutter.color_from_string("white"))
     background.set_size(SCREEN_WIDTH, SCREEN_HEIGHT)
+    #background.set_size(3000,2000)
     background.set_position(0, 0)
     self.group.add(background)
 
     title = "Print Queue for " + self.data["status"][2]["name"]
     feedtitleActor = clutter.Text()
     feedtitleActor.set_text(title)
-    feedtitleActor.set_font_name("serif 62")
+    feedtitleActor.set_font_name("DejaVu 64")
     feedtitleActor.set_color(clutter.color_from_string("black"))
-    feedtitleActor.set_size(SCREEN_WIDTH, 100)
+    feedtitleActor.set_size(SCREEN_WIDTH, 150)
     feedtitleActor.set_position(0, 0)
-    self.group.add(feedtitleActor) 
+    self.group.add(feedtitleActor)
     
     headers = {"id":"Job ID", "owner":"Owner", "title":"Title",
                "state":"Status", "physicaldest":"Printer"}
     
-    y = 100
+    y = 140
     y += self.add_entry_group(headers, y, width=SCREEN_WIDTH) + 10
     for entry in self.data["jobs"]:
       if y >= SCREEN_HEIGHT:
         break
-      y += self.add_entry_group(entry, y, width=1280) + 12 
+      y += self.add_entry_group(entry, y, width=SCREEN_WIDTH) + 12 
 
 
-  def add_entry_group(self, entry, starty, width=1280):
+  def add_entry_group(self, entry, starty, width=SCREEN_WIDTH):
+
+    # A dictionary which stores the clutter elements of this entry
+    dictentry = dict()
+
     # Add a rectangle that wraps the text indicating the status of
     # the given job
     statusrect = clutter.Rectangle()
@@ -87,62 +97,63 @@ class PrintDisplay(baseslide.BaseSlide):
       statusrect.set_color(clutter.color_from_string("orange"))
     else: #probably the title bar, no color needed
       statusrect.set_color(clutter.color_from_string("white"))
-    statusrect.set_position(80, starty - 5)
+    statusrect.set_position(50, starty - 5)
+
 
     title = clutter.Text()
-    title.set_font_name("serif 24")
+    title.set_font_name("DejaVu 24")
     title.set_text(entry["id"].__str__())
     title.set_width(width)
     title.set_color(clutter.color_from_string("black"))
-    title.set_position(100, starty)
+    title.set_position(75, starty)
+    dictentry["title"] = title
 
     # paste the status rectangle in using the correct height from the
     # title text.
     statusrect.set_size(SCREEN_WIDTH - 160, title.get_height() + 10)
-    self.group.add(statusrect)
+    dictentry["statusrect"] = statusrect
     # now add the title in, so it come in above the status rectangle
-    self.group.add(title)
 
     content = clutter.Text()
     content.set_text(entry["owner"])
-    content.set_font_name("serif 24")
+    content.set_font_name("DejaVu 24")
     content.set_line_wrap(True)
     content.set_line_wrap_mode(2)
     content.set_color(clutter.color_from_string("black"))
-    content.set_position(175, starty)
+    content.set_position(225, starty)
     content.set_width(width)
     content_height = content.get_height()
     content.set_ellipsize(3) 
-    self.group.add(content)
+    dictentry["content"] = content
 
     jobtitle = clutter.Text()
     jobtitle.set_text(entry["title"])
-    jobtitle.set_font_name("serif 24")
+    jobtitle.set_font_name("DejaVu 24")
     jobtitle.set_line_wrap(True)
     jobtitle.set_line_wrap_mode(2)
     jobtitle.set_color(clutter.color_from_string("black"))
-    jobtitle.set_position(300, starty)
+    jobtitle.set_position(450, starty)
     jobtitle.set_width(width)
     jobtitle_height = jobtitle.get_height()
     jobtitle.set_ellipsize(3) #Omit characters at the end of the text
-    self.group.add(jobtitle)
+    dictentry["jobtitle"] = jobtitle
 
     status = clutter.Text()
     status.set_text(entry["state"])
-    status.set_font_name("serif 24")
+    status.set_font_name("DejaVu 24")
     status.set_line_wrap(True)
     status.set_line_wrap_mode(2)
     status.set_color(clutter.color_from_string("black"))
-    status.set_position(500, starty)
+    status.set_position(900, starty)
     status.set_width(width)
     status_height = status.get_height()
     status.set_ellipsize(3) #Omit characters at the end of the text
-    self.group.add(status)
+    dictentry["status"] = status
 
     
     destination = clutter.Text()
     destination.set_text(entry["physicaldest"])
-    destination.set_font_name("serif 24")
+    destination.set_font_name("DejaVu 24")
     destination.set_line_wrap(True)
     destination.set_line_wrap_mode(2)
     if entry["physicaldest"] == "dali":
@@ -151,15 +162,29 @@ class PrintDisplay(baseslide.BaseSlide):
       destination.set_color(clutter.color_from_string("black"))
     else:
       destination.set_color(clutter.color_from_string("red"))
-    destination.set_position(650, starty)
+    destination.set_position(1200, starty)
     destination.set_width(width)
     destination_height = destination.get_height()
     destination.set_ellipsize(3) #Omit characters at the end of the text
-    self.group.add(destination)
+    dictentry["destination"]= destination
 
+    self.rows.append(dictentry)
     # Both items are oriented at the same height; 
     # only use the title height here
     return title.get_height()
+
+  def render(self):
+    """Renders the rows and colums from the rows object in this slide."""
+    for row in self.rows:
+      self.group.add(row["statusrect"])
+      self.group.add(row["content"])
+      self.group.add(row["destination"])
+      self.group.add(row["status"])
+      self.group.add(row["jobtitle"])
+      self.group.add(row["title"])
+
+  def getreqwidth(self, element):
+    """ Gets the column width needed for the given element. """
 
 def addBackground(self):
   stageBackground = clutter.Texture('feedimage.png')
@@ -170,7 +195,7 @@ def unescape(s):
   """ Replaces HTML entities with their unicode equivalent"""
   # unescape HTML code refs; c.f. http://wiki.python.org/moin/EscapingHtml
   return re.sub('&(%s);' % '|'.join(name2codepoint),
-            lambda m: unichr(name2codepoint[m.group(1)]), s)
+                lambda m: unichr(name2codepoint[m.group(1)]), s)
 
 def remove_html_tags(data):
   """ Removes HTML tags and unscapes the given string. """
