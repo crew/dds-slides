291a6941f2aeb57674ae4027490821e01d5d768c
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2010-02-04 13:41:33 -0500

    Clean up the print queue slide some more.
    Started to use Pango markup.

--- printqueue/layout.py
+++ printqueue/layout.py
@@ -1,4 +1,5 @@
 import cairo
+import pango
 import clutter
 import sys
 import baseslide
@@ -60,20 +61,11 @@ class PrintDisplay(baseslide.BaseSlide):
     # (our current preferred solution)
     self.group.add(self.background)
 
-    title = "Print Queue for " + self.data["status"][2]["name"]
-    feedtitleActor = clutter.Text()
-    feedtitleActor.set_text(title)
-    feedtitleActor.set_font_name("Garamond 64")
-    feedtitleActor.set_color(clutter.color_from_string("black"))
-    feedtitleActor.set_size(SCREEN_WIDTH, 150)
-    feedtitleActor.set_position(0, 0)
-    self.group.add(feedtitleActor)
-
     headers = {"state":"Status", "owner":"Owner", "title":"Title",
                 "physicaldest":"Printer"}
 
-    y = 110
-    y += self.add_entry_group(headers, y, width=SCREEN_WIDTH) + 10
+    y = 150
+#    y += self.add_entry_group(headers, y, width=SCREEN_WIDTH) + 10
     for entry in self.data["jobs"]:
       if y >= SCREEN_HEIGHT:
         break
@@ -85,7 +77,7 @@ class PrintDisplay(baseslide.BaseSlide):
     # a group which stores all the elements of this entry
     # (that is, it stores all the elements
     container = clutter.Group()
-    container.set_position(50, starty)
+    container.set_position(150, starty)
     container.set_width(1700)
     container.set_height(60)
     # Add a rectangle that wraps the text indicating the status of
@@ -150,22 +142,26 @@ class PrintDisplay(baseslide.BaseSlide):
     container.add(statusrect)
 
     content = clutter.Text()
-    content.set_text(entry["owner"])
-    content.set_font_name("Garamond 24")
+    ownerText = "%s" % entry["owner"]
+    content.set_font_name("Georgia 24")
+    content.set_text(ownerText)
+    content.set_use_markup(True)
     content.set_line_wrap(True)
     content.set_line_wrap_mode(2)
     content.set_color(clutter.color_from_string("black"))
-    content.set_position(100, 10)
+    content.set_position(125, 10)
     content.set_width(width)
     content_height = content.get_height()
     content.set_ellipsize(3)
     container.add(content)
 
     jobtitle = clutter.Text()
-    jobtitle.set_text(entry["title"])
-    jobtitle.set_font_name("Garamond 24")
+    jobtitle.set_font_name("Georgia 22")
+    titleText = "<i>%s</i>" % entry["title"]
+    jobtitle.set_text(titleText)
+    jobtitle.set_use_markup(True)
     jobtitle.set_line_wrap(True)
-    jobtitle.set_line_wrap_mode(2)
+    jobtitle.set_line_wrap_mode(0)
     jobtitle.set_color(clutter.color_from_string("black"))
     jobtitle.set_position(300, 10)
     jobtitle.set_width(width)
@@ -175,17 +171,19 @@ class PrintDisplay(baseslide.BaseSlide):
 
 
     destination = clutter.Text()
-    destination.set_text(entry["physicaldest"])
-    destination.set_font_name("Garamond 24")
+    destination.set_font_name("Georgia 24")
     destination.set_line_wrap(True)
     destination.set_line_wrap_mode(2)
+    destnText = ""
     if entry["physicaldest"] == "dali":
-      destination.set_color(clutter.color_from_string("blue"))
+      destnText = "<i>%s</i>" % entry["physicaldest"]
     elif entry["physicaldest"] == "Printer":
-      destination.set_color(clutter.color_from_string("black"))
+      destnText = "%s" % entry["physicaldest"]
     else:
-      destination.set_color(clutter.color_from_string("red"))
-    destination.set_position(1150, 10)
+      destnText = "<b>%s</b>" % entry["physicaldest"]
+    destination.set_text(destnText)
+    destination.set_use_markup(True)
+    destination.set_position(850, 10)
     destination.set_width(width)
     destination_height = destination.get_height()
     destination.set_ellipsize(3) #Omit characters at the end of the text
@@ -210,11 +208,24 @@ class PrintDisplay(baseslide.BaseSlide):
   def render(self):
     """Renders the rows and colums from the rows object in this slide."""
     rowContainer = clutter.Group()
+
+    # Make the title
+    title = "Print Queue for %s" % self.data["status"][2]["name"]
+    feedtitleActor = clutter.Text()
+    feedtitleActor.set_text(title)
+    feedtitleActor.set_use_markup(True)
+    feedtitleActor.set_font_name("Georgia Bold 42")
+    feedtitleActor.set_color(clutter.color_from_string("black"))
+    feedtitleActor.set_size(SCREEN_WIDTH, 150)
+    feedtitleActor.set_position(290, 50)
+
+    rowContainer.add(feedtitleActor)
+
     for row in self.rows:
       rowContainer.add(row)
 
     # Now rotate the container that holds the rows
-    rowContainer.move_by(175, 150)
+    rowContainer.move_by(250, 150)
     rowContainer.set_rotation(clutter.Z_AXIS, -3, 0, 0, 0)
     self.group.add(rowContainer)
 
