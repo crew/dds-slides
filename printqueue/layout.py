010ff011ac0ed9eac502b6ac52393e6d8d58547b
Will Nowak <compbrain@gmail.com>
2010-02-13 14:07:40 -0500

    PrintQueue updated to not add stuff every time

--- printqueue/layout.py
+++ printqueue/layout.py
@@ -19,27 +19,37 @@ class PrintDisplay(baseslide.BaseSlide):
     # Stores the information for all the elements of each row
     self.rows = list()
     self.dataURL = dataURL
+    self.setupmedia()
+  
+  def setupmedia(self):
     # Background
     self.background = clutter.Texture(filename='background.jpg')
     self.background.set_size(SCREEN_WIDTH, SCREEN_HEIGHT)
     self.background.set_position(0, 0)
+    self.group.add(self.background)
+    # Checkbox media
     self.checkmark = clutter.Texture(filename='checkmark.png')
     self.checkmark.set_size(40, 40)
     self.checkmark.hide()
     self.xmark = clutter.Texture(filename='xmark.png')
     self.xmark.set_size(40, 40)
     self.xmark.hide()
+    self.group.add(self.xmark)
+    self.group.add(self.checkmark)
 
   def event_beforeshow(self):
     self.refresh(self.dataURL)
-
+  
   def refresh(self, dataURL):
-    self.group.remove_all()
-    self.group.add(self.checkmark)
-    self.group.add(self.xmark)
     self.parsedata(dataURL)
     self.makeslide()
     self.render()
+
+  def event_afterhide(self):
+    self.rowContainer.destroy()
+    for x in self.rows:
+      self.rows.remove(x)
+      del x
 
   def parsedata(self, url):
     """ Parse data from the given URL, and populate data objects
@@ -47,25 +57,16 @@ class PrintDisplay(baseslide.BaseSlide):
     # use urllib to grab data from the url we're given
     webjson = urllib.urlopen(url)
     fdata = webjson.read() # the raw JSON from the printserver
-
     converter = json.JSONDecoder()
     pyjson = converter.decode(fdata)
-
     self.data = pyjson
-
-    # the data has been converted and put into an object...
 
   def makeslide(self):
     """ Adds the json print feed information to this slide. """
-    # Make a white rectangle to give the slide a white background
-    # (our current preferred solution)
-    self.group.add(self.background)
-
     headers = {"state":"Status", "owner":"Owner", "title":"Title",
                 "physicaldest":"Printer"}
 
     y = 150
-#    y += self.add_entry_group(headers, y, width=SCREEN_WIDTH) + 10
     for entry in self.data["jobs"]:
       if y >= SCREEN_HEIGHT:
         break
@@ -109,57 +110,38 @@ class PrintDisplay(baseslide.BaseSlide):
     self.roundedrec(context, 5, 5, 30, 30, 5)
     context.stroke()
 
-    del(context) # we need to destroy the context so that the
-                 # texture gets properly updated with the result
-                 # of our operations; you can either move all the
-                 # drawing operations into their own function and
-                 # let the context go out of scope or you can
-                 # explicitly destroy it
+    del(context)
 
     container.add(checkbox)
     container.show()
 
-    if entry["state"] == "completed":
-      if self.checkmark is None:
-        checkmark = self.checkmark = clutter.Texture(filename='checkmark.png')
-      else:
-        checkmark = clutter.Clone(self.checkmark)
-      checkmark.set_position(30, 10)
-      checkmark.set_width(checkbox.get_width() - 7)
-      checkmark.set_height(checkbox.get_height() - 7)
-      container.add(checkmark)
-
-    if entry["state"] == "canceled":
-      if self.xmark is None:
-        xmark = self.xmark = clutter.Texture(filename='xmark.png')
-      else:
-        xmark = clutter.Clone(self.xmark)
-      xmark.set_position(30, 10)
-      xmark.set_width(checkbox.get_width() - 7)
-      xmark.set_height(checkbox.get_height() - 7)
-      container.add(xmark)
+    if entry['state'] in ['completed', 'canceled']:
+      if entry['state'] == 'completed':
+        mark = clutter.Clone(self.checkmark)
+      elif entry['state'] == 'canceled':
+        mark = clutter.Clone(self.xmark)
+      mark.set_position(30, 10)
+      mark.set_width(checkbox.get_width() - 7)
+      mark.set_height(checkbox.get_height() - 7)
+      container.add(mark)
 
     container.add(statusrect)
 
     content = clutter.Text()
-    ownerText = "%s" % entry["owner"]
     content.set_font_name("Georgia 24")
-    content.set_text(ownerText)
+    content.set_text(entry['owner'])
     content.set_use_markup(True)
     content.set_line_wrap(True)
     content.set_line_wrap_mode(2)
     content.set_color(clutter.color_from_string("black"))
     content.set_position(125, 10)
     content.set_width(width)
-    content_height = content.get_height()
     content.set_ellipsize(3)
     container.add(content)
 
     jobtitle = clutter.Text()
     jobtitle.set_font_name("Georgia 22")
-    titleText = "<i>%s</i>" % entry["title"]
-    jobtitle.set_text(titleText)
-    jobtitle.set_use_markup(True)
+    jobtitle.set_markup("<i>%s</i>" % entry["title"])
     jobtitle.set_line_wrap(True)
     jobtitle.set_line_wrap_mode(0)
     jobtitle.set_color(clutter.color_from_string("black"))
@@ -178,11 +160,10 @@ class PrintDisplay(baseslide.BaseSlide):
     if entry["physicaldest"] == "dali":
       destnText = "<i>%s</i>" % entry["physicaldest"]
     elif entry["physicaldest"] == "Printer":
-      destnText = "%s" % entry["physicaldest"]
+      destnText = entry["physicaldest"]
     else:
       destnText = "<b>%s</b>" % entry["physicaldest"]
-    destination.set_text(destnText)
-    destination.set_use_markup(True)
+    destination.set_markup(destnText)
     destination.set_position(850, 10)
     destination.set_width(width)
     destination_height = destination.get_height()
@@ -192,7 +173,7 @@ class PrintDisplay(baseslide.BaseSlide):
     self.rows.append(container)
     return checkbox.get_height()
 
-  def roundedrec(self,context,x,y,w,h,r = 10):
+  def roundedrec(self, context, x, y, w, h, r=10):
     "Draw a rounded rectangle"
     context.move_to(x+r,y)                      # Move to A
     context.line_to(x+w-r,y)                    # Straight line to B
@@ -203,31 +184,29 @@ class PrintDisplay(baseslide.BaseSlide):
     context.curve_to(x,y+h,x,y+h,x,y+h-r)       # Curve to G
     context.line_to(x,y+r)                      # Line to H
     context.curve_to(x,y,x,y,x+r,y)             # Curve to A
-    return
 
   def render(self):
     """Renders the rows and colums from the rows object in this slide."""
-    rowContainer = clutter.Group()
+    self.rowContainer = clutter.Group()
 
     # Make the title
     title = "Print Queue for %s" % self.data["status"][2]["name"]
     feedtitleActor = clutter.Text()
     feedtitleActor.set_text(title)
-    feedtitleActor.set_use_markup(True)
     feedtitleActor.set_font_name("Georgia Bold 42")
     feedtitleActor.set_color(clutter.color_from_string("black"))
     feedtitleActor.set_size(SCREEN_WIDTH, 150)
     feedtitleActor.set_position(290, 50)
 
-    rowContainer.add(feedtitleActor)
+    self.rowContainer.add(feedtitleActor)
 
     for row in self.rows:
-      rowContainer.add(row)
+      self.rowContainer.add(row)
 
     # Now rotate the container that holds the rows
-    rowContainer.move_by(250, 150)
-    rowContainer.set_rotation(clutter.Z_AXIS, -3, 0, 0, 0)
-    self.group.add(rowContainer)
+    self.rowContainer.move_by(250, 150)
+    self.rowContainer.set_rotation(clutter.Z_AXIS, -3, 0, 0, 0)
+    self.group.add(self.rowContainer)
 
 app = PrintDisplay("http://queueviewer.ccs.neu.edu/printqueue/102/json/")
 slide = app.group
