6c0da46705f14184436fc343bb25b44da8a964e0
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2010-02-15 15:16:41 -0500

    Fixes Baskerville issues and some layout quirks. Includes new BG image.

--- campuscalendar/layout.py
+++ campuscalendar/layout.py
@@ -1,5 +1,6 @@
 # vim: set shiftwidth=4 tabstop=4 softtabstop=4 :
 import clutter
+import cairo
 import baseslide
 import config
 import logging
@@ -19,26 +20,26 @@ class CampusCalendar(baseslide.BaseSlide):
 
     def setuptext(self):
         self.eventtitle = clutter.Text()
-        self.eventtitle.set_font_name('Large Frys 48')
+        self.eventtitle.set_font_name('Baskerville 60')
         self.eventtitle.set_color(clutter.color_from_string('#ffffff'))
         self.eventtitle.set_size(1600, 1)
-        self.eventtitle.set_position(100, 200)
+        self.eventtitle.set_position(125, 225)
         self.eventtitle.set_ellipsize(3)
         self.eventtitle.set_depth(3)
         self.group.add(self.eventtitle)
 
         self.dateline = clutter.Text()
-        self.dateline.set_font_name('Large Frys 36')
+        self.dateline.set_font_name('Baskerville 52')
         self.dateline.set_color(clutter.color_from_string('#ffffff'))
-        self.dateline.set_position(100, 300)
+        self.dateline.set_position(160, 370)
         self.dateline.set_size(250, 1)
         self.dateline.set_depth(3)
         self.group.add(self.dateline)
 
         self.descblock = clutter.Text()
-        self.descblock.set_font_name('serif 24')
+        self.descblock.set_font_name('Baskerville 24')
         self.descblock.set_color(clutter.color_from_string('#ffffff'))
-        self.descblock.set_position(100, 450)
+        self.descblock.set_position(160, 475)
         self.descblock.set_size(1700, 550)
         self.descblock.set_ellipsize(3)
         self.descblock.set_depth(3)
@@ -72,9 +73,9 @@ class CampusCalendar(baseslide.BaseSlide):
         enddt = event.dtend.value.astimezone(self.localtime)
         start_date = startdt.strftime('%A, %m/%d %I:%M%p')
         end_date = enddt.strftime('%I:%M%p')
-        self.eventtitle.set_text(event.summary.value)
-        self.dateline.set_markup('<i>%s - %s</i>' % (start_date, end_date))
-        self.descblock.set_text(event.description.value)
+        self.eventtitle.set_markup('<span size="larger">%s</span>' % event.summary.value)
+        self.dateline.set_markup('<span size="smaller"><i>%s - %s</i></span>' % (start_date, end_date))
+        self.descblock.set_markup(event.description.value)
 
 app = CampusCalendar()
 slide = app.group
