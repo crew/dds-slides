ce1c6a1319d113ec85bb1a997457ffed72735abf
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2010-02-15 17:01:10 -0500

    Added a new fancy animation and design for when the client is unconfigured.

--- unconfigured/layout.py
+++ unconfigured/layout.py
@@ -1,38 +1,71 @@
 import clutter
 import baseslide
 import config
+from threading import Timer
 
 class Unconfigured(baseslide.BaseSlide):
     def __init__(self):
         baseslide.BaseSlide.__init__(self)
-        title = clutter.Text()
-        title.set_text("Unconfigured")
-        title.set_font_name("serif 71")
-        title.set_color(clutter.color_from_string("gold"))
-        title.set_size(1920, 100)
-        title.set_position(0, 0)
-        self.group.add(title)
-        
+        # Images
+        self.bg = clutter.Texture('bg.png')
+        self.blueprint = clutter.Texture('blueprint.png')
+        self.group.add(self.bg)
+        self.group.add(self.blueprint)
+        # Text
+        self.title = clutter.Text()
+        self.message = clutter.Text()
+        self.group.add(self.title)
+        self.group.add(self.message)
+
+    def event_beforeshow(self):
+        self.setupbg()
+        self.setuptext()
+
+    def event_aftershow(self):
+        t = Timer(5.0, self.animateitems)
+        t.start()
+
+    def setupbg(self):
+        self.bg.set_size(1920, 1080)
+        self.bg.set_position(0, 0)
+        self.bg.set_depth(1)
+
+        self.blueprint.set_size(1920, 1080)
+        self.blueprint.set_position(0, 0)
+        self.blueprint.set_depth(3)
+
+
+    def setuptext(self):
+        # Title
+        self.title.set_markup("<b>This client is unconfigured</b>")
+        self.title.set_font_name("sans 32")
+        self.title.set_color(clutter.color_from_string("white"))
+        self.title.set_size(1920, 100)
+        self.title.set_position(550, 740)
+        self.title.set_depth(2)
+
+        # Message
         jid = config.Option("client-jid")
         jid = jid.split('@')[0]
-        unconfigured = ('This client is unconfigured. '
-                        'Please use the DDS Master interface to add JID "'
-                        '%s'
-                        '" to a client group.' % jid)
-                        
-        message = clutter.Text()
-        message.set_text(unconfigured)
-        message.set_font_name("serif 48")
-        message.set_line_wrap(True)
-        message.set_color(clutter.color_from_string("white"))
-        message.set_size(1920, 30)
-        message.set_position(0, 120)
-        self.group.add(message)
+        unconfigured = ('Please use the DDS Web interface to add JID '
+                        '<i>%s</i>'
+                        ' to a client group.' % jid)
 
-        pic = clutter.Texture('lolcat_error.jpg')
-        pic.set_position(640,500)
-        self.group.add(pic)
-        
-    
+        self.message.set_markup(unconfigured)
+        self.message.set_font_name("sans 24")
+        self.message.set_line_wrap(True)
+        self.message.set_color(clutter.color_from_string("white"))
+        self.message.set_size(1400, 30)
+        self.message.set_position(300, 800)
+        self.message.set_depth(2)
+        # Show it when the animation happens
+        self.message.hide()
+
+
+    def animateitems(self):
+        self.blueprint.animate(clutter.EASE_OUT_SINE, 500, "width", 960, "height", 540, "x", 455, "y", 150)
+        self.message.show()
+
+
 app = Unconfigured()
 slide = app.group
