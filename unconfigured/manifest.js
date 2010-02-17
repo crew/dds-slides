10f23ed70d5396d2622f7eea5bc434029e2375b5
Will Nowak <compbrain@gmail.com>
2010-02-17 17:49:51 -0500

    Make the unconfigured slide last longer

--- unconfigured/manifest.js
+++ unconfigured/manifest.js
@@ -1,2 +1,2 @@
 {"transition" : "fade", "mode" : "module", "thumbnail_img": "_thumb.png",
- "title":"Unconfigured Splash", "duration":10, "priority":3}
+ "title":"Unconfigured Splash", "duration":30, "priority":3}
