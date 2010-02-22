42fe0023a91f422fd18fbf75c98c3a8d60f25e42
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2010-02-21 19:24:19 -0500

    Error slide inspired by 2001: A Space Odyssey.

---
+++ halfunction/layout.py
@@ -0,0 +1,43 @@
+import clutter
+import baseslide
+import config
+import gobject
+
+class HALfunction(baseslide.BaseSlide):
+    def __init__(self):
+        baseslide.BaseSlide.__init__(self)
+        self.redsquare = clutter.Texture('redsquare.png')
+        self.malfunction = clutter.Texture('glowtext.png')
+
+        self.group.add(self.redsquare)
+        self.group.add(self.malfunction)
+        self.setupanimation()
+
+    def event_beforeshow(self):
+        self.setupredsquare()
+
+    def event_aftershow(self):
+        self.tm.start()
+
+    def event_afterhide(self):
+        self.tm.stop()
+
+    def setupredsquare(self):
+        XYPOS = 173.5
+        self.redsquare.set_size(1575, 733)
+        self.redsquare.set_position(XYPOS, XYPOS)
+        self.redsquare.set_depth(1)
+
+        self.malfunction.set_size(1575, 733)
+        self.malfunction.set_position(XYPOS, XYPOS)
+        self.malfunction.set_depth(3)
+
+    def setupanimation(self):
+        self.tm = clutter.Timeline(duration=1200)
+        self.tm.set_loop(True)
+        self.alpha = clutter.Alpha(self.tm, clutter.EASE_IN_CUBIC)
+        self.behaviour = clutter.BehaviourOpacity(alpha=self.alpha, opacity_start=250, opacity_end=0x33)
+        self.behaviour.apply(self.malfunction)
+
+app = HALfunction()
+slide = app.group
