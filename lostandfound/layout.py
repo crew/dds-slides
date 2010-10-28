ccbb32f755ab38b643965fe850a2ca891da32fcb
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2010-10-28 15:31:37 -0400

    Lost and found now made to look pretty. Also moved the animation code back to a subroutine.

--- lostandfound/layout.py
+++ lostandfound/layout.py
@@ -8,40 +8,25 @@ class LostAndFound(baseslide.BaseSlide):
 
     def __init__(self):
         super(LostAndFound, self).__init__()
-        self.background = clutter.Texture('lost_and_found.png')
-        self.group.add(self.background)
-        self.background.set_size(1920, 1080)
-        self.background.set_position(0, 0)
-        # The text that says (upstairs)
-        t = clutter.Text()
-        t.set_font_name("sans serif 96")
-        t.set_text("(upstairs)")
-        t.set_color(clutter.color_from_string("white"))
-        t.set_position(600, 850)
-        self.group.add(t)
-        self.upstairs = t
-        # This is copied from halfunction.
-        # Makes the text blink.
-        self.tm = None
-        self.tm = clutter.Timeline(duration=2000)
-        self.tm.set_loop(True)
-        self.alpha = clutter.Alpha(self.tm, clutter.EASE_IN_CUBIC)
-        self.behaviour = clutter.BehaviourOpacity(alpha=self.alpha,
-            opacity_start=0xFF, opacity_end=0x00)
-        self.behaviour.apply(t)
+        self.background = clutter.Texture('Lost.jpg')
+        self.blurtext = clutter.Texture('Lost-Blur.jpg')
 
-    def event_beforeshow(self):
-        return True
+        self.group.add(self.background)
+        self.group.add(self.blurtext)
+        self.setupanimation()
 
     def event_aftershow(self):
         self.tm.start()
 
-    def event_beforehide(self):
-        pass
-
     def event_afterhide(self):
         self.tm.stop()
 
+    def setupanimation(self):
+        self.tm = clutter.Timeline(duration=2500)
+        self.tm.set_loop(False)
+        self.alpha = clutter.Alpha(self.tm, clutter.EASE_IN_CUBIC)
+        self.behaviour = clutter.BehaviourOpacity(alpha=self.alpha, opacity_start=0, opacity_end=250)
+        self.behaviour.apply(self.blurtext)
 
 app = LostAndFound()
 slide = app.group
