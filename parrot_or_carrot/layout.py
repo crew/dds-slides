3826e6b00eca4bb17933999ea2a414231541f474
Shawn Smith <shawn930@zerowing.ccs.neu.edu>
2011-02-09 17:25:42 -0500

    URL needed to have rn=number, not n=number, removed tm.set_loop

--- parrot_or_carrot/layout.py
+++ parrot_or_carrot/layout.py
@@ -3,22 +3,21 @@ import baseslide
 import config
 import gobject
 from urllib import urlretrieve
-from random import choice
 
 class ParrotOrCarrot(baseslide.BaseSlide):
     def __init__(self):
         baseslide.BaseSlide.__init__(self)
-        self.zoomed_image = urllib.urlretrieve("http://parrotorcarrot.com/dds.php?n=1337&fresh=true", "/var/tmp/zoomed.jpg")
-        self.actual_image = urllib.urlretrieve("http://parrotorcarrot.com/dds.php?n=1337", "/var/tmp/actual.jpg")
+        self.zoomed_image = urlretrieve("http://parrotorcarrot.com/dds.php?rn=1337&fresh=true", "/var/tmp/zoomed.jpg")
+        self.actual_image = urlretrieve("http://parrotorcarrot.com/dds.php?rn=1337", "/var/tmp/actual.jpg")
         self.zoomed = clutter.Texture('/var/tmp/zoomed.jpg')
         self.actual = clutter.Texture('/var/tmp/actual.jpg')
 
         self.group.add(self.zoomed)
         self.group.add(self.actual)
-        self.setupanimation()
+        self.setup_animation()
 
     def event_beforeshow(self):
-        self.
+        self.setup_images()
 
     def event_aftershow(self):
         self.tm.start()
@@ -26,7 +25,7 @@ class ParrotOrCarrot(baseslide.BaseSlide):
     def event_afterhide(self):
         self.tm.stop()
 
-    def setupzoomed(self):
+    def setup_images(self):
         XYPOS = 173.5
         self.zoomed.set_size(1575, 733)
         self.zoomed.set_position(XYPOS, XYPOS)
@@ -36,12 +35,12 @@ class ParrotOrCarrot(baseslide.BaseSlide):
         self.actual.set_position(XYPOS, XYPOS)
         self.actual.set_depth(3)
 
-    def setupanimation(self):
+    def setup_animation(self):
         self.tm = clutter.Timeline(duration=1200)
-        self.tm.set_loop(True)
+#        self.tm.set_loop(False)
         self.alpha = clutter.Alpha(self.tm, clutter.EASE_IN_CUBIC)
-        self.behaviour = clutter.BehaviourOpacity(alpha=self.alpha, opacity_start=250, opacity_end=0x33)
-        self.behaviour.apply(self.actual)
+        self.behaviour = clutter.BehaviourOpacity(alpha=self.alpha, opacity_start=250, opacity_end=0)
+        self.behaviour.apply(self.zoomed)
 
 app = ParrotOrCarrot()
 slide = app.group
