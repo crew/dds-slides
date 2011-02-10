6652f0fe57a403c3ebea58a819ac0d45a17cd2c3
Nick Aldwin <nick@aldwin.us>
2011-02-09 23:12:22 -0500

    Add title text for Parrot or Carrot

--- parrot_or_carrot/layout.py
+++ parrot_or_carrot/layout.py
@@ -5,6 +5,9 @@ import gobject
 import time
 from urllib import urlretrieve
 
+SCREEN_HEIGHT = 1200
+SCREEN_WIDTH = 1920
+
 class ParrotOrCarrot(baseslide.BaseSlide):
     def __init__(self):
         baseslide.BaseSlide.__init__(self)
@@ -15,9 +18,15 @@ class ParrotOrCarrot(baseslide.BaseSlide):
         self.zoomed = clutter.Texture('/var/tmp/zoomed.jpg')
         self.actual = clutter.Texture('/var/tmp/actual.jpg')
 
+        self.title = clutter.Text()
+        self.title.set_text("Parrot or Carrot?")
+        self.title.set_font_name("Serif Bold 56")
+        self.title.set_color(clutter.color_from_string("white"))
+
 	self.setup_images()
         self.group.add(self.zoomed)
         self.group.add(self.actual)
+        self.group.add(self.title)
         self.setup_animation()
 
     def event_aftershow(self):
@@ -35,6 +44,9 @@ class ParrotOrCarrot(baseslide.BaseSlide):
         self.actual.set_size(1575, 733)
         self.actual.set_position(XYPOS, XYPOS)
         self.actual.set_depth(1)
+
+        self.title.set_position(SCREEN_WIDTH/2-self.title.get_width()/2, 45)
+        self.title.set_depth(3)
 
     def setup_animation(self):
         self.tm = clutter.Timeline(duration=2000)
