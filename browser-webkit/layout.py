1cb4a20b279bb6fceea8fc380458b843e1414ff1
Roderic Morris <roderyc@gmail.com>
2011-02-22 14:48:17 -0500

    Use WebkitSlide from the crew.dds.contrib.browser library.

--- browser-webkit/layout.py
+++ browser-webkit/layout.py
@@ -2,28 +2,7 @@ import logging
 import threading
 import clutter
 from crew.dds import baseslide
-from crew.dds.contrib import browser
-
-class WebkitSlide(baseslide.BaseSlide):
-
-    def __init__(self, url):
-        super(self.__class__, self).__init__()
-        self.url = url
-        self.browser = None
-
-    def event_beforeshow(self):
-        self.browser = browser.WebBrowser(self.url, show_toolbar=False)
-        self.browser.fullscreen()
-
-    def event_aftershow(self):
-        pass
-
-    def event_beforehide(self):
-        pass
-
-    def event_afterhide(self):
-        self.browser.destroy()
-
+from crew.dds.contrib.browser import WebkitSlide
 
 app = WebkitSlide('http://pitcam.ccs.neu.edu')
 slide = app.group
